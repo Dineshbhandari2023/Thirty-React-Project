fd8fdf158c1c7e74ac20b3f14a87243b770842bc
Dineshbhandari2023 <iamdinesh.github@gmail.com>
2024-07-15 00:04:39 +0545

    New clone project started using raect and tailwindCSS

---
+++ clone/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import asset1 from "../assets/asset 1.jpeg";
+
+const Hero = () => {
+  return (
+    <div className="grid grid-cols-2 bg-bg ">
+      <div className="z-10 mt-20 ml-96 bg-gradient-to-r from-primary to-secondary h-[500px] w-[400px] justify-center rounded-lg">
+        <h3 className="pl-14 justify-center mt-40 text-6xl font-semibold tracking-wider">
+          Adivasi <br /> Herbal
+          <br /> Hair Oil
+        </h3>
+        <button className="ml-14 mt-7 px-5 py-2 border border-black rounded-md bg-black text-primary transition duration-300 hover:bg-black hover:shadow-[0_0_0_5px_black] ">
+          Shop Now
+        </button>
+      </div>
+
+      <div className="flex pt-14 pr-40 justify-end text-center ">
+        <img
+          className=" h-[550px] w-[1000px] rounded-lg shadow-2xl tracking-wider"
+          src={asset1}
+          alt="backgroundimg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
