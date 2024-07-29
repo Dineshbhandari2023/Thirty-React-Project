2ac96afcd41e2e431ac4a48b3ae9a8db60e82f1c
Dineshbhandari2023 <iamdinesh.github@gmail.com>
2024-07-29 23:16:09 +0545

    Zoom clone using NextJs and tailwindCSS

---
+++ Zoom/constants/index.ts
@@ -0,0 +1,22 @@
+const sidebarLinks = [
+    {
+        imgUrls:"icons/Home.svg",
+        route: "/",
+        label:"Home"
+    },
+    {
+        imgUrls:"icons/upcoming.svg",
+        route: "/upcoming",
+        label:"Upcoming"
+    },
+    {
+        imgUrls:"icons/previous.svg",
+        route: "/previous",
+        label:"Previous"
+    },
+    {
+        imgUrls:"icons/recordings.svg",
+        route: "/recordings",
+        label:"Recordings"
+    }
+]
